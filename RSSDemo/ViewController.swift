8221d070cf848df89d6117836a841b3dcb12fd74
Satish Kumar <satishios25@gmail.com>
2017-06-16 22:31:22 +0530

    Initial Commit

---
+++ RSSDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSSDemo
+//
+//  Created by Satish Kumar on 16/06/17.
+//  Copyright © 2017 SatishKumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
